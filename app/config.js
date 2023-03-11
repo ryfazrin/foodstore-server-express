07356763cd0360ef260cc82fa2ac92280750fddc
ryfazrin <riansera515@gmail.com>
2023-03-11 23:43:59 +0700

    fix type

--- app/config.js
+++ app/config.js
@@ -7,7 +7,7 @@ module.exports = {
   //----- konfigurasi database ----//
   dbHost: process.env.DB_HOST,
   dbUser: process.env.DB_USER,
-  dbPort: process.env_DB_PORT,
+  dbPort: process.env.DB_PORT,
   dbPass: process.env.DB_PASS,
   dbName: process.env.DB_NAME
 }
