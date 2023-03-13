8d1785660940243a24f70f849ade24b907e0e9f7
ryfazrin <riansera515@gmail.com>
2023-03-13 16:03:40 +0700

    updated: product router post with multer

--- app/products/router.js
+++ app/products/router.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
+const multer = require('multer');
 
 const productController = require('./controller');
 
-router.post('/products', productController.store);
+router.post('/products', multer().none(), productController.store);
 
 module.exports = router;
