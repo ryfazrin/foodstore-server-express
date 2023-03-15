450bc32a1f7cb25bec2c63e24082dfced48597db
ryfazrin <riansera515@gmail.com>
2023-03-15 20:59:39 +0700

    added: store tag

---
+++ app/tags/controller.js
@@ -0,0 +1,26 @@
+const Tag = require('./model');
+
+async function store(req, res, next) {
+  try {
+    let payload = req.body;
+    let tag = new Tag(payload);
+
+    await tag.save();
+
+    return res.json(tag);
+  } catch (err) {
+    if (err && err.name === 'ValidationError') {
+      return res.json({
+        error: 1,
+        message: err.message,
+        fields: err.errors
+      });
+    }
+
+    next(err);
+  }
+}
+
+module.exports = {
+  store
+}
