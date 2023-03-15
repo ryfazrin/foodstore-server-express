8af7a47fe3c90fd9979f3e9a4a132cc4b9df35dc
ryfazrin <riansera515@gmail.com>
2023-03-15 21:00:00 +0700

    created: router post tag

---
+++ app/tags/router.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const multer = require('multer');
+
+const tagController = require('./controller');
+
+router.post('/tags', multer().none(), tagController.store);
+
+module.exports = router;
