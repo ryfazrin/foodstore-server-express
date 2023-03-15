2d8bcec511d56c4c30ed44e765e7fa23a9f058b9
ryfazrin <riansera515@gmail.com>
2023-03-15 21:03:47 +0700

    added: update and destroy tag

--- app/tags/controller.js
+++ app/tags/controller.js
@@ -21,6 +21,37 @@ async function store(req, res, next) {
   }
 }
 
+async function update(req, res, next) {
+  try {
+    let payload = req.body;
+
+    let tag = await Tag.findOneAndUpdate({ _id: req.params.id }, payload,
+      { new: true, runValidators: true });
+
+    return res.json(tag);
+  } catch (err) {
+    if (err && err.name === 'ValidationError') {
+      return res.json({
+        error: 1,
+        message: err.message,
+        fields: err.errors
+      });
+    }
+    next(err);
+  }
+}
+
+async function destroy(req, res, next) {
+  try {
+    let tag = await Tag.findOneAndDelete({ _id: req.params.id });
+    return res.json(tag); // <---
+  } catch (err) {
+    next(err);
+  }
+}
+
 module.exports = {
-  store
+  store,
+  update,
+  destroy
 }
