06e3b6dec2a2d44cb90bf40ae647d90cddb2b49e
ryfazrin <riansera515@gmail.com>
2023-03-15 20:41:43 +0700

    created: store category

---
+++ app/categories/controller.js
@@ -0,0 +1,26 @@
+const Category = require('./model');
+
+async function store(req, res, next) {
+  try {
+    let payload = req.body;
+    let category = new Category(payload);
+
+    await category.save();
+
+    return res.json(category);
+  } catch (err) {
+    if (err && err.name === 'ValidationError') {
+      return res.json({
+        error: 1,
+        message: err.message,
+        fields: err.errors
+      })
+    }
+
+    next(err);
+  }
+}
+
+module.exports = {
+  store
+}
