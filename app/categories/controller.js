d7e314f18e0aab8c27000b2ec31f23face916cf5
ryfazrin <riansera515@gmail.com>
2023-03-15 20:46:09 +0700

    added: update category

--- app/categories/controller.js
+++ app/categories/controller.js
@@ -21,6 +21,30 @@ async function store(req, res, next) {
   }
 }
 
+async function update(req, res, next) {
+  try {
+    let payload = req.body;
+    let category = await Category.findOneAndUpdate(
+      { _id: req.params.id },
+      payload,
+      { new: true, runValidators: true }
+    );
+
+    return res.json(category);
+  } catch (err) {
+    if (err && err.name === 'ValidationError') {
+      return res.json({
+        error: 1,
+        message: err.message,
+        fields: err.errors
+      });
+    }
+
+    next(err);
+  }
+}
+
 module.exports = {
-  store
+  store,
+  update
 }
