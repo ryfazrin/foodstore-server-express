75d1f1cd326137cc323166568942f837908f564b
ryfazrin <riansera515@gmail.com>
2023-03-26 23:24:44 +0700

    created: local Strategy

--- app/auth/controller.js
+++ app/auth/controller.js
@@ -1,3 +1,6 @@
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 const User = require('../user/model');
 
 async function register(req, res, next) {
@@ -20,6 +23,31 @@ async function register(req, res, next) {
   }
 }
 
+async function localStrategy(email, password, done) {
+  try {
+    // (1) cari user ke MongoDB
+    let user =
+      await User
+        .findOne({ email })
+        .select('-__v -createdAt -updatedAt -cart_items -token');
+
+    // (2) jika user tidak ditemukan, akhiri proses login
+    if (!user) return done();
+
+    if (bcrypt.compareSync(password, user.password)) {
+      ({ password, ...userWithoutPassword } = user.toJSON());
+      // (4) akhiri pengecekkan, user berhasil login
+      // berikan data user tanpa password
+      return done(null, userWithoutPassword);
+    }
+  } catch (err) {
+    done(err, null);
+  }
+
+  done();
+}
+
 module.exports = {
-  register
+  register,
+  localStrategy
 }
--- app/auth/router.js
+++ app/auth/router.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
 const multer = require('multer');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 const controller = require('./controller');
+
+passport.use(new LocalStrategy({ usernameField: 'email' }, controller.localStrategy));
 
 router.post('/register', multer().none(), controller.register);
 
