85af3fd171562f566a8ab26f5b40369a63eebf4c
ryfazrin <riansera515@gmail.com>
2023-03-26 23:07:40 +0700

    added: Hashing password at Mongoose Hooks

--- app/user/model.js
+++ app/user/model.js
@@ -1,5 +1,8 @@
+const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
 const { model, Schema } = mongoose;
+
+const HASH_ROUND = 10;
 
 let userSchema = Schema({
   full_name: {
@@ -29,22 +32,21 @@ let userSchema = Schema({
   token: [String]
 }, { timestamps: true });
 
+userSchema.pre('save', function (next) {
+  this.password = bcrypt.hashSync(this.password, HASH_ROUND);
+  next();
+});
+
 userSchema.path('email').validate(function (value) {
-  // (1) email regular expression
   const EMAIL_RE = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-  // (2) test email, hasilnya adalah `true` atau `false`
-  // jika ternyata `true` maka validasi berhasil
-  // jika ternyata `false` maka validasi gagal
+
   return EMAIL_RE.test(value);
 }, attr => `${attr.value} harus merupakan email yang valid!`);
 
 userSchema.path('email').validate(async function (value) {
   try {
-    // (1) lakukan pencarian ke _collection_ User berdasarkan `email`
     const count = await this.model('User').count({ email: value });
-    // (2) kode ini mengindikasikan bahwa jika user ditemukan akan mengembalikan `false` jika tidak ditemukan mengembalikan `true`
-    // jika `false` maka validasi gagal
-    // jika `true` maka validasi berhasil
+
     return !count;
   } catch (err) {
     throw err
